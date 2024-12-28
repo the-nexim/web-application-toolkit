0cb365d2c1a4b44f2d8e89c4f4ac016b922ad78c
arashagp <arash.qardashpoor@gmail.com>
2024-12-28 14:53:01 +0330

    feat(eleventy): add date and time formatting utilities and a trim function
    Co-authored-by: S. Amir Mohammad Najafi <njfamirm@gmail.com>

---
+++ packages/eleventy-config/src/lib/util/trim.js
@@ -0,0 +1,9 @@
+/**
+ * Trims the given content.
+ *
+ * @param {string} content - The content to trim.
+ * @returns {string} - The trimmed content.
+ */
+export function trim(content) {
+  return content.trim();
+}
